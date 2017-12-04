61ea429cfaaec71ec3b296fa06b44cf7b31c1fd1
Markus J. Wetzel <markuswetzel@gmx.net>
2017-12-04 22:51:41 +0100

    Fix date format polyfill

--- src/utils/polyfillDateFormat.js
+++ src/utils/polyfillDateFormat.js
@@ -3,7 +3,7 @@ import isString from './isString';
 export default function polyfillDateFormat(variable) {
   if (isString(variable)) {
     // replace dashes with slashes for IE and Safari support
-    const response = variable.replace(/-/g, '/');
+    const response = variable.replace(/(.*)-(.*)-(.*) (.*)/g, '$1/$2/$3 $4');
     return response;
   }
 
