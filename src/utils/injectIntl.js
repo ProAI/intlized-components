85fb0d4d171dd7c059ef0268db6a7a100a40d7bb
Markus J. Wetzel <markuswetzel@gmx.net>
2019-04-24 09:47:41 +0200

    Fix timeAgo function

--- src/utils/injectIntl.js
+++ src/utils/injectIntl.js
@@ -26,8 +26,39 @@ export default function injectIntl(state) {
     timeAgo: (value, options) => {
       if (!value) return null;
 
+      const secondsPerMinute = 60;
+      const secondsPerHour = 3600; // secondsPerMinute * 60
+      const secondsPerDay = 86400; // secondsPerHour * 24
+      const secondsPerWeek = 604800; // secondsPerHour * 7
+      const secondsPerMonth = 2629800; // secondsPerDay * (365.25 / 12)
+      const secondsPerYear = 31557600; // secondsPerDay * 365.25
+
+      const diff =
+        (new Date().getTime() - new Date(polyfillDateFormat(value)).getTime()) /
+        1000;
+
       const instance = new Intl.RelativeTimeFormat(state.intl.locale, options);
-      return instance.format(new Date(polyfillDateFormat(value)));
+
+      if (diff < secondsPerMinute) {
+        return instance.format(-Math.round(diff), 'second');
+      }
+      if (diff < secondsPerHour) {
+        return instance.format(-Math.round(diff / secondsPerMinute), 'minute');
+      }
+      if (diff < secondsPerDay) {
+        return instance.format(-Math.round(diff / secondsPerHour), 'hour');
+      }
+      if (diff < secondsPerWeek) {
+        return instance.format(-Math.round(diff / secondsPerDay), 'day');
+      }
+      if (diff < secondsPerMonth) {
+        return instance.format(-Math.round(diff / secondsPerWeek), 'week');
+      }
+      if (diff < secondsPerYear) {
+        return instance.format(-Math.round(diff / secondsPerMonth), 'month');
+      }
+
+      return instance.format(-Math.round(diff / secondsPerYear), 'year');
     },
     locale: state.intl.locale,
   };
